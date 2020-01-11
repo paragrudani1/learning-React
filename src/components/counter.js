402fd82eb455dc2dcc41575ce818c3bb5e11657e
paragrudani1 <47742372+paragrudani1@users.noreply.github.com>
2020-01-11 10:56:39 +0530

    first commit

---
+++ src/components/counter.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+        tags : ['tag1', 'tag2', 'tag3']
+    };
+
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this)
+    // }
+    
+    // handleIncrement() {
+    //     console.log("increment Clicked", this);
+    // }
+
+
+    handleIncrement = () => {
+        console.log('Increment Clicked', this);
+    }
+
+    renderTags() {
+        if (this.state.tags.length === 0) return "there is no tags"
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul> 
+    }
+
+
+    render() {
+        return(
+    <div>
+        <span className={this.getBadgeClasses()}>
+            {this.formateCount()}
+        </span>
+        
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">
+            Increment
+        </button>
+
+        {this.renderTags()}
+    </div>
+
+        )
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formateCount() {
+        const { count } = this.state;
+
+        return count === 0 ? "Zero" : count;
+    }
+}
+
+export default Counter;
